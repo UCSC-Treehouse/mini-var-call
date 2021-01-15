c423c7106cb13de2fb1186df5605b794e4394528
hbeale <hbeale@gmail.com>
2021-01-15 09:09:08 -0800

    updating bed file version and usage

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+default_bed_file=/app/Treehouse_variants_to_call.2021-01-13_11.29.27.bed
+
 fasta=$1
 bam=$2
 outputs=$3
+bed_file=${4:-$default_bed_file}
 
 set 
 for file in "$@"
@@ -14,7 +17,7 @@ done
 
 echo Running freebayes on $bam
 freeBayesSettings="--dont-left-align-indels --pooled-continuous --pooled-discrete -F 0.03 -C 2"
-/app/freebayes/bin/freebayes --targets /app/th_precise_merged.bed $freeBayesSettings -f $fasta $bam >  /tmp/mini.vcf
+/app/freebayes/bin/freebayes --targets ${bed_file} $freeBayesSettings -f $fasta $bam >  /tmp/mini.vcf
 
 echo Running snpEff on $bam
 snpEffSettings="-nodownload -noNextProt -noMotif -noStats -classic -no PROTEIN_PROTEIN_INTERACTION_LOCUS -no PROTEIN_STRUCTURAL_INTERACTION_LOCUS"
